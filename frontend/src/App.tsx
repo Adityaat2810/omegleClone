65a3a082118135d29d3d875c04e9c6903e53a160
aditya <adityasainics10@gmail.com>
2024-03-01 18:38:35 +0530

    set up the project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing } from './components/landing';
+import { Room } from './components/room'
+function App() {
+ 
+
+  return (
+    <>
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/room" element={<Room />} />
+
+      </Routes>
+    </BrowserRouter>
+     
+    </>
+  )
+}
+
+export default App
